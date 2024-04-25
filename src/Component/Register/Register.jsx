8547cca937cd07de666846f709fadc0924707dd5
dymamunbd <mdmamunfedev@gmail.com>
2024-04-25 22:32:05 +0600

    home slider done

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -1,7 +1,20 @@
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="bg-white shadow-lg w-[600px] mx-auto mt-8">
+        <h1 className="text-2xl text-black fonr-bold p-4 text-center">Register Now</h1>
+        <div className="mt-7">
+            <form
+            
+            >
+                <input className="p-2 outline-none border-[1px] border-orange-500" type="text" placeholder="Name" /> <br />
+                <input className="p-2 outline-none border-[1px] border-orange-500" type="email" placeholder="Email" /> <br />
+                <input className="p-2 outline-none border-[1px] border-orange-500" type="text" placeholder="Name" /> <br />
+
+
+            </form>
+        </div>
+    </div>
   )
 }
 
